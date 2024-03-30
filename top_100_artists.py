4d19fbc41a6f0a8b8de830890e3e4fbaf47555be
yurioneix <yurioneix18@gmail.com>
2024-03-29 23:22:18 -0300

    Corrige identação com Black

--- top_100_artists.py
+++ top_100_artists.py
@@ -3,8 +3,7 @@ import csv
 
 with open("billboard200.csv", encoding="utf-8") as file:
     billboard200_reader = csv.reader(file)
-    
+
     header, *data = billboard200_reader
 
     print(header)
-
