615d4e81f3842f6f87a66fb52672bc62ff8c2952
yurioneix <yurioneix18@gmail.com>
2024-03-31 00:57:03 -0300

    Transforma os valores de Weeks in Charts com hifen para 0

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-for row in data:
-    #     billboard_dict = {header: data for header, data in zip(header, data)}
-
-    # print(billboard_dict)
--- top_100_artists.py
+++ top_100_artists.py
@@ -1,11 +1,23 @@
 import csv
 
-
 with open("billboard200.csv", encoding="utf-8") as file:
     billboard200_reader = csv.reader(file)
 
     header, *data = billboard200_reader
 
+    billboard_list = []
     for row in data:
         billboard_dict = {k: v for (k, v) in zip(header, row)}
-        print(billboard_dict)
+        if billboard_dict["Weeks in Charts"] == "-":
+            billboard_dict["Weeks in Charts"] = 0
+        billboard_list.append(billboard_dict)
+    print(billboard_list[0])
+    # billboard_sorted = sorted(
+    #     billboard_list, key=lambda x: x["Weeks in Charts"],
+    #     reverse=True
+    #     )
+    # print("primeiro", billboard_list[0])
+    # print("último", billboard_sorted[len(billboard_sorted) - 1])
+    # print("sem sort", billboard_list[len(billboard_list) - 1])
+    # billboard_list.sort(key=lambda x: x["Weeks in Charts"])
+    # print("com sorte", billboard_list[len(billboard_list) - 1])
