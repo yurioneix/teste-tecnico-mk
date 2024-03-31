908ee3a2bcd9c2e737f1c61d78a1316eb29c633c
yurioneix <yurioneix18@gmail.com>
2024-03-31 02:05:55 -0300

    Remove chaves desnecessárias do dicionário

--- top_100_artists.py
+++ top_100_artists.py
@@ -5,19 +5,32 @@ with open("billboard200.csv", encoding="utf-8") as file:
 
     header, *data = billboard200_reader
 
-    billboard_list = []
-    for row in data:
-        billboard_dict = {k: v for (k, v) in zip(header, row)}
-        if billboard_dict["Weeks in Charts"] == "-":
-            billboard_dict["Weeks in Charts"] = 0
-        billboard_list.append(billboard_dict)
-    print(billboard_list[0])
-    # billboard_sorted = sorted(
-    #     billboard_list, key=lambda x: x["Weeks in Charts"],
-    #     reverse=True
-    #     )
-    # print("primeiro", billboard_list[0])
-    # print("último", billboard_sorted[len(billboard_sorted) - 1])
-    # print("sem sort", billboard_list[len(billboard_list) - 1])
-    # billboard_list.sort(key=lambda x: x["Weeks in Charts"])
-    # print("com sorte", billboard_list[len(billboard_list) - 1])
+billboard_list = []
+for row in data:
+    billboard_dict = {k: v for (k, v) in zip(header, row)}
+    billboard_dict.pop("Date")
+    billboard_dict.pop("Image URL")
+    billboard_dict.pop("Last Week")
+    if billboard_dict["Weeks in Charts"] == "-":
+        billboard_dict["Weeks in Charts"] = 0
+    billboard_dict["Weeks in Charts"] = int(
+        billboard_dict["Weeks in Charts"]
+        )
+    billboard_list.append(billboard_dict)
+billboard_sorted = sorted(
+    billboard_list, key=lambda x: x["Weeks in Charts"], reverse=True
+)
+# desired_order_list = [
+#     "Rank",
+#     "Song",
+#     "Artist",
+#     "Peak Position",
+#     "Weeks in Charts"
+#     ]
+# reordered_list = []
+# for row in billboard_sorted:
+#     reordered_dict = {k: billboard_sorted[k] for k in desired_order_list}
+#     reordered_list.append(reordered_dict)
+print(billboard_sorted[0:99])
+# print("primeiro", billboard_sorted[0])
+# print("último", billboard_sorted[len(billboard_sorted) - 1])
