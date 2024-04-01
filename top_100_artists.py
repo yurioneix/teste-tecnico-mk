c308cba0ea4a6a422e9dba4c6a2748eb51a6f65a
yurioneix <yurioneix18@gmail.com>
2024-04-01 12:11:06 -0300

    Escreve as top100 faixas da Billboard no arquivo csv

--- top_100_artists.py
+++ top_100_artists.py
@@ -13,13 +13,23 @@ for row in data:
     billboard_dict.pop("Last Week")
     if billboard_dict["Weeks in Charts"] == "-":
         billboard_dict["Weeks in Charts"] = 0
-    billboard_dict["Weeks in Charts"] = int(
-        billboard_dict["Weeks in Charts"]
-        )
+    billboard_dict["Weeks in Charts"] = int(billboard_dict["Weeks in Charts"])
     billboard_list.append(billboard_dict)
 billboard_sorted = sorted(
     billboard_list, key=lambda x: x["Weeks in Charts"], reverse=True
 )
+
+keys = billboard_sorted[0].keys()
+
+with open("top100faixas.csv", "w", encoding="utf-8") as top_100:
+    dict_writer = csv.DictWriter(top_100, keys)
+
+    headers = ["Rank", "Song", "Artist", "Peak Position", "Weeks in Charts"]
+    dict_writer.writeheader()
+    dict_writer.writerows(billboard_sorted[0:100])
+    # for dict in billboard_sorted:
+    #     for key, value in dict.items():
+
 # desired_order_list = [
 #     "Rank",
 #     "Song",
@@ -31,6 +41,6 @@ billboard_sorted = sorted(
 # for row in billboard_sorted:
 #     reordered_dict = {k: billboard_sorted[k] for k in desired_order_list}
 #     reordered_list.append(reordered_dict)
-print(billboard_sorted[0:99])
+# print(billboard_sorted[0:100])
 # print("primeiro", billboard_sorted[0])
 # print("último", billboard_sorted[len(billboard_sorted) - 1])
